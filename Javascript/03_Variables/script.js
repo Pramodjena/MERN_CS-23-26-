743c50fe86571f70720893896ba4915d20795679
Pramodjena <pramoodjena@gmail.com>
2025-07-01 23:45:54 +0530

    mern updated

---
+++ Javascript/03_Variables/script.js
@@ -0,0 +1,15 @@
+// Variable :
+// console.log(myAge);
+// console.log(myName);
+
+// var myName;
+// let myAge;
+// const myCity = "Bhubaneswar";
+// console.log(myName);
+// console.log(myAge);
+// console.log(myCity);
+
+{
+  let myMark = 90;
+  console.log(myMark);
+}
